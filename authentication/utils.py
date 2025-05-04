e119e387195e26cc27284aa497e13e6d030a6bc2
laraibhussain1109 <134272774+laraibhussain1109@users.noreply.github.com>
2025-05-04 10:54:48 +0530

    Create utils.py
    To be used in future

---
+++ authentication/utils.py
@@ -0,0 +1,20 @@
+
+import random
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_otp_email(request, user):
+    code = f"{random.randint(0, 999999):06d}"
+    subject = "Your TrueCollabs login code"
+    message = (
+        f"Hi {user.first_name},\n\n"
+        f"Your one‑time login code is: {code}\n\n"
+        "This code will expire in 5 minutes.\n\n"
+        "If you didn’t request this, please ignore."
+    )
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+    # store in session
+    request.session['otp_user_pk'] = user.pk
+    request.session['otp_code']    = code
+    request.session['otp_expires'] = (timezone.now() + timedelta(minutes=5)).timestamp()
