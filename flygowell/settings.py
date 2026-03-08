3c3838819686f7c1fb24a87e6d18cc7fac0a9fbb
laraibhussain1109 <134272774+laraibhussain1109@users.noreply.github.com>
2026-03-08 18:02:01 +0530

    Use explicit AppConfig entries for local apps

--- flygowell/settings.py
+++ flygowell/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'campaign',
-    'authentication',
+    'campaign.apps.CampaignConfig',
+    'authentication.apps.AuthenticationConfig',
 
     # Add your custom apps here
 ]
